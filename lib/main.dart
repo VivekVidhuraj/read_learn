5283deef60bb401573c58094a948994f23d2b524
vivekvraj <login.vivekvidhuraj@gmail.com>
2024-08-16 11:36:20 +0530

    14 august work email and password authentication

--- lib/main.dart
+++ lib/main.dart
@@ -34,3 +34,23 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+//
+// import 'package:flutter/material.dart';
+// import 'package:read/cliprectsamples/cliprectt.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//
+//       home: const CliprectSample(),
+//     );
+//   }
+// }
+
